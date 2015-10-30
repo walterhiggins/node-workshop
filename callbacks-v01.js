92378b2b7669d00e87e04b97943c8534e53313e8
Walter Higgins <walter.higgins@gmail.com>
2015-10-30 16:19:47 +0000

    Create callbacks-v01.js

---
+++ callbacks-v01.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+var source = './';
+fs.readdir(source, function(err, files){
+  if (err) {
+    console.log('Error finding files:' + err);
+  } else {
+    files.forEach( function( filename ){
+      fs.stat( filename, function( err, stats ){
+        if (err) {
+          console.log('Stat error:' + err);
+        } else {
+          if ( stats.isFile() ){
+            fs.readFile( filename, function( err, contents ){
+              if (err){
+                console.log('Read Error:' + err);
+              } else {
+                console.log(filename + ':' + contents.length);
+              }
+            });
+          }
+        }
+      });
+    });
+  }
+}); 
