39bbc5f796c484a1285c0bc45027621c9a0e160b
Walter Higgins <walter.higgins@gmail.com>
2015-11-24 11:02:18 +0000

    Create hello-server.js

---
+++ hello-server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+function onRequest(request, response) {
+  response.writeHead(200, {
+    "Content-Type": "text/plain"
+  });
+  response.end("Hello World\n");
+}
+var server = http.createServer( onRequest );
+server.listen(8000);
